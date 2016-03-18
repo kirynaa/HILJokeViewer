a07740026161b2b2fc07d2205f9ed8275cce85f8
DenDoronin <den.doronind@gmail.com>
2016-03-18 19:17:23 +0200

    Initial Commit

---
+++ HILJokeViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HILJokeViewer
+//
+//  Created by Doronin Denis on 3/18/16.
+//  Copyright © 2016 Hillel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HILJokeViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HILJokeViewer
+//
+//  Created by Doronin Denis on 3/18/16.
+//  Copyright © 2016 Hillel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
